095d03d650ac6bb96746060cc03d8b3d83a5b682
Jussi Parviainen <jparviainen@exafore.com>
2018-11-15 21:10:30 +0200

    cancel button in dialog

--- gui_star.m
+++ gui_star.m
@@ -1,12 +1,8 @@
 function gui_star
+%GUI_STAR Make Start
 folder = 'C:\Users\Mörököllit\Pictures\2018-10-29 leikkimokki\';
 
-% Make figure, axes and plot map
-hfig = figure('Position',[100,50,1400,700]);
-handleaxes = axes('Parent', hfig,...
-    'Units','Pixels',...
-    'Position',[350,100,1000,550]);
-
+% Initialize variables that are shared with subfunctions
 file_list = [];
 nFiles = 0;
 path_list = [];
@@ -16,6 +12,16 @@ max_g =0;
 max_b =0;
 im = [];
 weight = 1;
+
+% Make figure, axes and plot map
+hfig = figure('Position',[100,50,1400,700]);
+h_axes = axes('Parent', hfig,...
+    'Units','Pixels',...
+    'Position',[270,100,1000,550]);
+h_axes.XTick = [];
+h_axes.YTick = [];
+
+
 h_load = uicontrol('Parent',hfig,...
     'Style','pushbutton',...
     'String', 'Load Images', ...
@@ -24,6 +30,7 @@ h_load = uicontrol('Parent',hfig,...
     'Callback', @(src,eventdata)load_callback(src,eventdata) );
 
 h_stack = uicontrol('Parent',hfig,...
+    'Enable','off',...
     'Style','pushbutton',...
     'String', 'Stack', ...
     'Value', 0, ...
@@ -31,6 +38,7 @@ h_stack = uicontrol('Parent',hfig,...
     'Callback', @(src,eventdata)stack_callback(src,eventdata) );
 
 h_save = uicontrol('Parent',hfig,...
+    'Enable','off',...
     'Style','pushbutton',...
     'String', 'Save', ...
     'Value', 0, ...
@@ -38,18 +46,26 @@ h_save = uicontrol('Parent',hfig,...
     'Callback', @(src,eventdata)save_callback(src,eventdata) );
 
 weight_list = {'normal','fade in','fade out','fade in&out'};
-
 % Plot dropdown menu for weight
 h_weight = uicontrol(hfig,'Style','popupmenu');
 h_weight.Position = [150 400, 100, 100];
 h_weight.String = weight_list;
 h_weight.Callback = @weight_callback;
 
+% Normalize all units, objects resize automatically
+h_load.Units = 'normalized';
+h_save.Units = 'normalized';
+h_stack.Units = 'normalized';
+h_weight.Units = 'normalized';
+h_axes.Units = 'normalized';
+hfig.Units = 'normalized';
+
+
 
 % Load images Callback
     function load_callback(~,~)
         [file_list, path_list] = uigetfile(fullfile(folder, '.jpg'), ...
-            'All Files (*.*)','MultiSelect','on');
+            'JPG Files (*.jpg)','MultiSelect','on');
         im = imread(fullfile(path_list,file_list{1}));
         imres = size(im);
         r_im = im(:,:,1);
@@ -59,25 +75,40 @@ h_weight.Callback = @weight_callback;
         max_g = g_im(:);
         max_b = b_im(:);
         weight = ones(size(max_b));
-        imshow(im, 'Parent', handleaxes)
+        imshow(im, 'Parent', h_axes)
         nFiles = numel(file_list);
+        if nFiles > 1
+            h_save.Enable = 'on';
+            h_stack.Enable = 'on';
+            h_weight.Enable = 'on';
+        end
     end
 
 
 % Load images Callback
     function stack_callback(~,~)
-        if nFiles < 2
-            errordlg('Load at least 2 images')
-            return;
-        end
+
         im = imread(fullfile(path_list,file_list{1}));
         max_r =max_r*0;
         max_g =max_g*0;
         max_b =max_b*0;
-        h_wait = waitbar(0,'stacking');
+        
+        message = sprintf('Stacking %i/%i',0,nFiles);
+        h_wait = waitbar(0,message, ...
+            'CreateCancelBtn','setappdata(gcbf,''canceling'',1)');
+        setappdata(h_wait,'canceling',0);
         for i = 1:nFiles
             im = imread(fullfile(path_list,file_list{i}));
-            h_wait = waitbar(i/nFiles,h_wait);
+            message = sprintf('Stacking %i/%i',i,nFiles);
+            % Check whether user closed the wait message
+            if ~isvalid(h_wait)
+                message = sprintf('Stacking %i/%i',i,nFiles);
+                h_wait = waitbar(0,message);
+            end
+            if getappdata(h_wait,'canceling')
+                break
+            end
+            h_wait = waitbar(i/nFiles,h_wait,message);
             r_im = im(:,:,1);
             g_im = im(:,:,2);
             b_im = im(:,:,3);
@@ -87,7 +118,7 @@ h_weight.Callback = @weight_callback;
             im(:,:,1) = reshape(max_r,imres(1),imres(2));
             im(:,:,2) = reshape(max_g,imres(1),imres(2));
             im(:,:,3) = reshape(max_b,imres(1),imres(2));
-            imshow(im, 'Parent', handleaxes)
+            imshow(im, 'Parent', h_axes)
         end
         delete(h_wait)
     end
